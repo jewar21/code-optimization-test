d87b15f15eac86080edd8e7dd9644725e8bec243
niksoftar7 <nicollprueba@gmail.com>
2024-09-13 14:04:30 -0500

    Refactor complete for quiz

--- src/AgeValidation.java
+++ src/AgeValidation.java
@@ -1,13 +1,21 @@
+import java.util.Scanner;
+
 public class AgeValidation {
     public static void main(String[] args) {
-        int age = 18;
-        
-        if (age >= 18) {
+
+        final int AGELIMIT = 18;
+        Scanner input = new Scanner(System.in);
+        int age = input.nextInt();
+        if (age> AGELIMIT) {
             System.out.println("Access granted");
         }
         
-        if (age < 18) {
+        else if (age < AGELIMIT && age > AGELIMIT) {
             System.out.println("Access denied");
         }
+        else{
+            System.out.println("invalid number");
+        }
+        input.close();
     }
 }
