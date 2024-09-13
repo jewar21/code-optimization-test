ebae607bb0a0530fe4212b3036adc67fe068101c
xiomara lizcano <xlizcanoc@ufpso.edu.co>
2024-09-13 13:58:58 -0500

    solucion quiz refactirizacion xiomara lizcano 192396

--- src/Store.java
+++ src/Store.java
@@ -1,15 +1,24 @@
 public class Store {
     public static void main(String[] args) {
-        int p1 = 15;
-        int p2 = 10;
-        int p3 = 5;
+        //precio de los productos//
+        int p1 = 20;
+        int p2 = 15;
+        int p3 = 10;
 
-        int total1 = p1 * 2;
-        int total2 = p2 * 3;
-        int total3 = p3 * 4;
+         // Cantidad de cada producto vendido
+        int cantidad1 = 2;
+        int cantidad2 = 3;
+        int cantidad3 = 4;
 
+        //calcula el total de ventas de cada producto//
+        int total1 = p1 * cantidad1;
+        int total2 = p2 * cantidad2;
+        int total3 = p3 * cantidad3;
+
+        //calcula el total de ventas generadas//
         int totalSales = total1 + total2 + total3;
 
+        //evalua el desempeño de las ventas//
         if (totalSales > 50) {
             System.out.println("Good sales performance");
         } else {
