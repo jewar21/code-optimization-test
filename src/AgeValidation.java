f243aa77cf48189fdb1164c2a80dd1b5c47982da
SergioDF07 <sadiazf@ufpso.edu.co>
2024-09-13 13:49:57 -0500

    solucion quiz refactorizacion Sergio Diaz 192357

--- src/AgeValidation.java
+++ src/AgeValidation.java
@@ -1,13 +1,23 @@
+/* importamos el scanner al codigo */
+import java.util.Scanner;
 public class AgeValidation {
     public static void main(String[] args) {
-        int age = 18;
-        
-        if (age >= 18) {
-            System.out.println("Access granted");
-        }
-        
-        if (age < 18) {
-            System.out.println("Access denied");
+        /* Asignamos una constante para el limite de edad */
+        final int LIMIT_AGE = 18 ;
+        try ( /* Declaramos la variable scanner */ Scanner scanner = new Scanner( System.in )) {
+            int age ;
+            /* Le preguntamos al usuario cual es su edad */
+            System.out.println("What is your age?");
+            /* usamos el scanner definido anteriormente para guardar el dato que digite el usuario */
+            age = scanner.nextInt();
+            /* nos deshacemos de la redundancia de los dos if usando un if-else */
+            if (age > LIMIT_AGE ) {
+                System.out.println("Access granted"); 
+            } else if (age <= LIMIT_AGE & age>0) {
+                System.out.println("Access denied");
+            } else {
+                System.out.println("Unexisting age");/* Aqui limitamos los valores para que si se llega a ingresar un valor menor a cero aparezca el mensaje de edad inexistente */
+            }
         }
     }
 }
