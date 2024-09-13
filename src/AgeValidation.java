fcf65c08e389c76c7cb028cfb92a0d1f90f2723e
JhayDarko <jhojanderropero55@gmail.com>
2024-09-13 13:56:03 -0500

    Refactor complete for quiz

--- src/AgeValidation.java
+++ src/AgeValidation.java
@@ -1,13 +1,28 @@
+import java.util.Scanner;
+
 public class AgeValidation {
     public static void main(String[] args) {
-        int age = 18;
-        
-        if (age >= 18) {
+
+        //Definición de constantes
+        final int AGE_LIMIT = 18;
+        final int VALID_AGE = 0;
+
+        //Inicialización del escaner para pedir datos al ususario
+        try (Scanner scan = new Scanner(System.in)) {
+            System.out.println("Ingresa tu edad");
+            int age = scan.nextInt();
+            
+            //Validación de la edad
+            if (age < VALID_AGE) {
+                System.out.println("Invalid Age");
+            }
+            //Comprobar el caso correspondiente y salida de texto
+            else if (age >= AGE_LIMIT) {
             System.out.println("Access granted");
-        }
-        
-        if (age < 18) {
+            }
+            else {
             System.out.println("Access denied");
+            }
         }
     }
 }
--- src/Store.java
+++ src/Store.java
@@ -1,16 +1,24 @@
 public class Store {
     public static void main(String[] args) {
-        int p1 = 15;
-        int p2 = 10;
-        int p3 = 5;
 
-        int total1 = p1 * 2;
-        int total2 = p2 * 3;
-        int total3 = p3 * 4;
+        //Definición de constantes
+        final int SALES_TRESHOLD = 50;
+        final int MAX_PRODUCTS = 3;
 
-        int totalSales = total1 + total2 + total3;
+        //Definición de los arrelos con precios y cantitades
+        int[] prices = {15, 10, 5};
+        int[] quantities = {2, 3, 4};
 
-        if (totalSales > 50) {
+        //Inicialización del contador con el total de ventas
+        int totalSales = 0;
+
+        //Ciclo para realizar las operaciones entre arreglos
+        for (int i = 0; i < MAX_PRODUCTS; i++) {
+            totalSales += prices[i] * quantities[i];
+        }
+
+        //Verificación y salida de texto
+        if (totalSales >= SALES_TRESHOLD) {
             System.out.println("Good sales performance");
         } else {
             System.out.println("Low sales performance");
